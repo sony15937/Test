d5c2f07c512a4a8f4a8ea355f3f5eea3fe232edf
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-07 17:44:48 +0800

    update layout page

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,14 +1,48 @@
 <template>
-    <div style="height: 100%">
+    <el-container :class="classObj">
+        <div/>
+        <sidebar />
+        <el-container>
+            <el-header>
+                <nav-bar />
+            </el-header>
+            <el-main>
+                <app-main/>
+            </el-main>
+        </el-container>
         <el-button @click="logout">
             asd
         </el-button>
-    </div>
+    </el-container>
 </template>
 
 <script>
+    import AppMain from './components/appmain'
+    import NavBar from './components/navbar'
+    import Sidebar from './components/sidebar'
     export default {
         name: "Layout",
+        components:{
+            AppMain,
+            NavBar,
+            Sidebar
+        },
+        computed:{
+            sidebar(){
+                return this.$store.state.app.sidebar
+            },
+            device(){
+                return this.$store.state.app.device
+            },
+            classObj(){
+                return{
+                    hideSidebar: !this.sidebar.opened,
+                    openSidebar: this.sidebar.opened,
+                    withoutAnimation: this.sidebar.withoutAnimation,
+                    mobile: this.device === 'mobile'
+                }
+            }
+        },
         methods:{
             logout(){
                 this.$store.dispatch('user/logout')
