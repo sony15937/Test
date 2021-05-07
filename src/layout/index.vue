83d05d2a37f13ea08373f0d2ac41a722277070d4
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-07 16:54:21 +0800

    add layout and home index page

---
+++ src/layout/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="height: 100%">
+        <el-button @click="logout">
+            asd
+        </el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Layout",
+        methods:{
+            logout(){
+                this.$store.dispatch('user/logout')
+                this.$router.push('/login')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div style="height: 100%">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
