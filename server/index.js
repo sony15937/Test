2cf54cddfc119a5843df1816d8fa0f52478bd1ce
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-07 11:06:37 +0800

    add mongo connect plug-in

--- server/index.js
+++ server/index.js
@@ -2,10 +2,19 @@ const express = require('express')
 const path = require("path")
 const app = express()
 
+const mongoose = require('mongoose')
+const mongoConfig = require('./config/mongoConfig')
+
 app.use(require('cors')())
 app.use(express.json())
-
 app.use(express.static(path.join(__dirname,'dist')))
+
+mongoose.connect(mongoConfig,{
+    useCreateIndex:true,
+    useFindAndModify:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
 
 app.listen(3001,()=>{
     console.log("http://localhost:3001/")
