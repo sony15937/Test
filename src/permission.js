94ee00252c8ac12770ecafe2dcd2071832f68f32
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-07 16:28:08 +0800

    edit router 路徑跳轉控置

--- src/permission.js
+++ src/permission.js
@@ -1,5 +1,20 @@
 import router from "./router";
+import Cookie from 'js-cookie'
 
 router.beforeEach((to, from, next) => {
-    next()
+    const role = Cookie.get('app_user_role')
+    if(role){
+        if(to.path === '/login')
+            next('/')
+        else if(!to.meta.role || to.meta.role.includes(role))
+            next()
+        else
+            next('/')
+    }
+    else{
+        if(to.path === '/login')
+            next()
+        else
+            next('/login')
+    }
 })
