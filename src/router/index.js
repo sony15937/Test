e5b0772451429f0ad87507f64b3da0e4a2053f5b
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-07 16:27:24 +0800

    delete unUsed router and add login router

--- src/router/index.js
+++ src/router/index.js
@@ -6,17 +6,14 @@ Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/login',
+    name:'Login',
+    component: () => import('../views/login')
+  },
+  {
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
