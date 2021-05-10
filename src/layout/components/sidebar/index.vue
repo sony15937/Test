12df27afc890483d2777ec714a0a4d3b9b4875a9
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-10 14:02:47 +0800

    add sidebar component

--- src/layout/components/sidebar/index.vue
+++ src/layout/components/sidebar/index.vue
@@ -1,11 +1,58 @@
 <template>
-    <div>
-    </div>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+        <el-menu
+                :default-active="getNowActive"
+                :unique-opened="false"
+                :collapse-transition="false"
+                mode="vertical"
+                @select="handleOpen"
+                :collapse="isCollapse"
+                active-text-color="#ffd04b"
+                text-color="#fff"
+                background-color="#304156">
+            <el-menu-item :index="menuItemRoute[0]">
+                <svg-icon svg-name="user"/>
+                <span slot="title">Home</span>
+            </el-menu-item>
+        </el-menu>
+    </el-scrollbar>
 </template>
 
 <script>
     export default {
-        name: "index"
+        name: "Sidebar",
+        data(){
+            return {
+                menuItemRoute:[
+                    this.$router.options.routes[1].path + this.$router.options.routes[1].children[0].path //Home
+                ]
+            }
+        },
+        computed:{
+            getRole(){
+                return this.$store.state.user.userRole
+            },
+            isCollapse(){
+                return !this.$store.state.app.sidebar.opened
+            },
+            getNowActive(){
+                if(this.menuItemRoute.includes(this.$route.path))
+                    return this.$route.path
+                else{
+                    for(let i=0;i<this.menuItemRoute.length;i++){
+                        if(this.$route.path.indexOf(this.menuItemRoute[i])!==-1)
+                            return this.menuItemRoute[i]
+                    }
+                    return this.$route.path
+                }
+            }
+        },
+        methods:{
+            handleOpen(key){
+                if(key !== this.$route.path)
+                    this.$router.push({path:key})
+            }
+        }
     }
 </script>
 
