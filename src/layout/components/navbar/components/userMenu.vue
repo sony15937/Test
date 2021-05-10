2c30902488c64b1fddd6a9cb2dba360aba0df829
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-10 14:07:34 +0800

    add navBar-userMenu component

---
+++ src/layout/components/navbar/components/userMenu.vue
@@ -0,0 +1,33 @@
+<template>
+    <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+            <el-avatar shape="square" src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif" class="user-avatar"/>
+        </div>
+        <el-dropdown-menu slot="dropdown" class="user-dropdown">
+            <router-link to="/">
+                <el-dropdown-item>
+                    Home
+                </el-dropdown-item>
+            </router-link>
+            <el-dropdown-item divided @click.native="logout">
+                <span style="display:block;">Log Out</span>
+            </el-dropdown-item>
+        </el-dropdown-menu>
+    </el-dropdown>
+</template>
+
+<script>
+    export default {
+        name: "userMenu",
+        methods:{
+            async logout(){
+                await this.$store.dispatch('user/logout')
+                this.$router.push({path:'/login'})
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
