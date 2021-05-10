d51858ce9ce030453484e5ee142cbec905d5338b
sony15937 <101316207@gms.tcu.edu.tw>
2021-05-10 14:07:14 +0800

    add navBar component

--- src/layout/components/navbar/index.vue
+++ src/layout/components/navbar/index.vue
@@ -1,14 +1,42 @@
 <template>
     <div>
+        <i v-if="isCollapse" class="el-icon-s-unfold" @click="toggleSidebar"/>
+        <i v-else class="el-icon-s-fold" @click="toggleSidebar"/>
+        <div class="right-menu">
+            <lang-menu/>
+            <user-menu/>
+        </div>
     </div>
 </template>
 
 <script>
+    import userMenu from "./components/userMenu";
+    import langMenu from "./components/langMenu";
     export default {
-        name: "index"
+        name: "NavBar",
+        components:{
+            userMenu,
+            langMenu
+        },
+        computed:{
+            isCollapse(){
+                return !this.$store.state.app.sidebar.opened
+            }
+        },
+        methods:{
+            toggleSidebar(){
+                this.$store.dispatch('app/toggleSidebar')
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .el-icon-s-fold,.el-icon-s-unfold{
+        margin-left: 0;
+        margin-top: 10px;
+        float: left;
+        font-size: 25px;
+        cursor: pointer;
+    }
 </style>
